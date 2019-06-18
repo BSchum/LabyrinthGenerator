95657cc263088979a7708222fa694d7dc3297603
brice schumacher <brice.schumacher@ynov.com>
2019-06-18 22:40:29 +0200

    Larger cell + camera

--- Assets/MazeManager.cs
+++ Assets/MazeManager.cs
@@ -12,28 +12,32 @@ public class MazeManager : MonoBehaviour
     Room[,] _maze;
     public GameObject cell;
 
-    List<Cell> cells = new List<Cell>();
     // Start is called before the first frame update
     void Start()
     {
         LabyrinthGenerator.LabyrinthGenerator generator = new LabyrinthGenerator.LabyrinthGenerator();
-        _maze = generator.Generate(10, 10, 80);
+        _maze = generator.Generate(5, 5, 20);
 
-        for (int x = _maze.GetLength(1) - 1; x >= 0; x--)
+
+        for (int y = 0; y < _maze.GetLength(1); y++)
         {
-            for (int y = 0; y < _maze.GetLength(0); y++)
+            for (int x = 0; x < _maze.GetLength(0); x++)
             {
                 if (_maze[x, y].IsVisited)
                 {
-                    GameObject go = Instantiate(cell, new Vector3(x * 3, 0, y * 3), Quaternion.identity);
+                    GameObject go = Instantiate(cell, new Vector3(x *3 * 5, 0, y*3 * 3), Quaternion.identity);
                     Cell newCell = go.GetComponent<Cell>();
                     newCell.x = x;
                     newCell.y = y;
+                    go.name = $"Cell {newCell.x},{newCell.y}";
 
-                    cells.Add(newCell);
                     if (_maze[x, y].IsFirst)
                     {
                         go.ChangeColor(Color.green);
+                    }
+                    else
+                    {
+                        go.GetComponentInChildren<Camera>().gameObject.SetActive(false);
                     }
 
                     if (_maze[x, y].IsLast)
@@ -45,7 +49,6 @@ public class MazeManager : MonoBehaviour
                     {
                         if (dir == Direction.None)
                             continue;
-                        Debug.Log(dir);
                         if (!((_maze[x, y].Doors & dir) == dir))
                         {
                             Destroy(newCell.walls.First(wall => wall.direction == dir).wall);
